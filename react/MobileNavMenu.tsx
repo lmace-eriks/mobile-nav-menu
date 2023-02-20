db1acdf009767f4d457e40f9fa01fe3923d74ef1
Web Developer <useraccount@WebDeveloperMBP.local>
2023-02-20 12:29:53 -0600

    Reformatting and trimming fat - LM

--- react/MobileNavMenu.tsx
+++ react/MobileNavMenu.tsx
@@ -1,102 +1,101 @@
-import React, { ReactChildren, useRef } from 'react';
-import { useEffect, useState } from 'react';
+import React, { ReactChildren, useRef, useEffect, useState } from 'react';
+// import { canUseDOM } from 'vtex.render-runtime';
 
 // Styles
 import styles from "./styles.css";
 
 interface MobileNavMenuProps {
-  children: ReactChildren
+  children: ReactChildren | any
 }
 
 const MobileNavMenu: StorefrontFunctionComponent<MobileNavMenuProps> = ({ children }) => {
+  const openGate = useRef(true);
   const [openNav, setOpenNav] = useState<boolean>(false);
-  const navRef = useRef(null);
-  const overlayRef = useRef(null);
-
-  // @ts-expect-error
-  const miniCart = children[0];
-
-  const childrenBelowCart: any = [];
-
-  // @ts-expect-error
-  for (let i = 0; i < children.length; i++) {
-    if (i > 0) {
-      // @ts-expect-error
-      childrenBelowCart.push(children[i]);
-    }
-  }
+  const [miniCart, setMiniCart] = useState<any>(children[0]);
+  const navRef = useRef<any>(!null);
+  const overlayRef = useRef<any>(!null);
+  const [childrenInDrawer, setChildrenInDrawer] = useState<any>();
 
   useEffect(() => {
-    // console.clear();
-  })
+    if (!openGate.current) return;
+    openGate.current = false;
+
+    const tempChildrenInDrawer: any = [];
+
+    // [0] child is Mini Cart and should not be in the drawer - LM
+    children.forEach((child: any, index: number) => {
+      if (index) tempChildrenInDrawer.push(child);
+    });
+
+    setChildrenInDrawer(tempChildrenInDrawer);
+  });
 
   const handleOpenNav = () => {
     setOpenNav(true);
 
+    // Timeout accounts for CSS transition / animation - LM
     setTimeout(() => {
-      // @ts-expect-error
       navRef.current.style.left = 0;
-
-      // @ts-expect-error
       overlayRef.current.style.opacity = 0.5;
     }, 100);
-
   }
 
   const handleCloseNav = () => {
-    // @ts-expect-error
     navRef.current.style.left = "-100%";
-
-    // @ts-expect-error
     overlayRef.current.style.opacity = 0;
 
+    // Timeout accounts for CSS transition / animation - LM
     setTimeout(() => {
       setOpenNav(false);
     }, 250);
   }
 
+  const OpenNavButton = () => (
+    <div className={styles.hamburgerContainer}>
+      <button aria-label="Open Navigation" onClick={handleOpenNav} className={styles.hamburgerWrapper}>
+        <div className={styles.hamburgerLine}></div>
+        <div className={styles.hamburgerLine}></div>
+        <div className={styles.hamburgerLine}></div>
+      </button>
+    </div>
+  );
+
+  const EriksLogo = () => (
+    <a className={styles.logoLink} href="/">
+      <img src="https://eriksbikeshop.vtexassets.com/arquivos/eriks-logo.svg" alt="Logo / Home Link" loading="eager" className={styles.mobileLogo} />
+    </a>
+  );
+
+  const ShoppingCart = () => (
+    <div className={styles.miniCartContainer}>
+      {miniCart}
+    </div>
+  );
+
+  const MobileNavOpenState = () => (<>
+    <nav ref={navRef} className={styles.navigationContainer}>
+      <button aria-label="Close Navigation" onClick={handleCloseNav} className={styles.closeMobileNavButton}>X</button>
+      <div className={styles.childrenContainer}>
+        {childrenInDrawer}
+      </div>
+    </nav>
+    <div onClick={handleCloseNav} ref={overlayRef} className={styles.mobileNavOverlay}></div>
+  </>);
+
   return (
     <div className={styles.mobileNavMenuContainer}>
-      <div className={styles.hamburgerContainer}>
-        <div onClick={handleOpenNav} className={styles.hamburgerWrapper}>
-          <div onClick={handleOpenNav} className={styles.hamburgerLine}></div>
-          <div onClick={handleOpenNav} className={styles.hamburgerLine}></div>
-          <div onClick={handleOpenNav} className={styles.hamburgerLine}></div>
-        </div>
-      </div>
-      <div className={styles.logoContainer}>
-        <div className={styles.logoWrapper}>
-          <a href="/"><img src="https://eriksbikeshop.vtexassets.com/arquivos/logo.png" alt="Erik's Bike Shop" loading="lazy" className={styles.mobileLogo} /></a>
-        </div>
-      </div>
-      <div className={styles.miniCartContainer}>
-        <div className={styles.miniCartWrapper}>
-          {miniCart}
-        </div>
-      </div>
-      {openNav &&
-        <>
-          <nav ref={navRef} className={styles.navigationContainer}>
-            <div className={styles.navigationWrapper}>
-              <div onClick={handleCloseNav} className={styles.closeMobileNavButton}>X</div>
-              <div className={styles.childrenContainer}>
-                <div className={styles.childrenWrapper}>
-                  {childrenBelowCart}
-                </div>
-              </div>
-            </div>
-          </nav>
-          <div onClick={handleCloseNav} ref={overlayRef} className={styles.mobileNavOverlay}></div>
-        </>
-      }
+      <OpenNavButton />
+      <EriksLogo />
+      <ShoppingCart />
+      {openNav && <MobileNavOpenState />}
     </div>
-  )
+  );
 }
 
 MobileNavMenu.schema = {
-  title: 'editor.mobilenavmenu.title',
-  description: 'editor.mobilenavmenu.description',
-  type: 'object',
+  title: "Mobile Navigation Menu",
+  description: "",
+  type: "object",
   properties: {}
 }
 
